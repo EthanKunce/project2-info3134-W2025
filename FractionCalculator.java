cf53085d1f95e1e1f29afc354cf0dad47dcb12c7
Ethan Kunce <ethankunce@ymail.com>
2025-04-02 21:23:46 -0400

    Ethan's boilerplate

---
+++ FractionCalculator.java
@@ -0,0 +1,11 @@
+/*
+ * Author:		Ethan Kunce
+ * Date:		02 April 2025
+ * ProgramName:	FractionCalculator
+ * Desc:		Boiler Plate Text
+ */
+
+public class FractionCalculator {
+    
+
+} // End Class
